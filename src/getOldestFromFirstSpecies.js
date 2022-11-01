6df5d6039b7451194389467008c9f5389d0ebca6
Jefferson Donizete do Nascimento <jdn1937@hotmail.com>
2022-11-01 10:00:56 -0300

    Requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,14 @@
-const data = require('../data/zoo_data');
+const { species, employees } = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const especie = employees.find((servidor) => servidor.id === id).responsibleFor[0];
+  const especieId = species.find((element) => element.id === especie);
+  const residente = especieId.residents;
+  const maisVelho = residente.reduce((acc, curr) => (acc.age > curr.age ? acc : curr));
+  const array = [];
+  array.push(maisVelho.name, maisVelho.sex, maisVelho.age);
+  return array;
 }
 
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 module.exports = getOldestFromFirstSpecies;
