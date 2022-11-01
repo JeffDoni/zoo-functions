2308c019b557b7536f0be00df24cbb1575eabf33
Jefferson Donizete do Nascimento <jdn1937@hotmail.com>
2022-11-01 00:01:27 -0300

    requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,26 @@
-const data = require('../data/zoo_data');
+const { prices } = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const child = entrants.filter((element) => element.age < 18).length;
+  const adult = entrants.filter((element) => element.age >= 18 && element.age < 50).length;
+  const senior = entrants.filter((element) => element.age >= 50).length;
+
+  const age = {
+    child,
+    adult,
+    senior,
+  };
+  return age;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui.
+  if (!entrants || Object.values(entrants).length === 0) {
+    return 0;
+  }
+  const child = prices.child * countEntrants(entrants).child;
+  const adult = prices.adult * countEntrants(entrants).adult;
+  const senior = prices.senior * countEntrants(entrants).senior;
+  return child + adult + senior;
 }
 
 module.exports = { calculateEntry, countEntrants };
