c72c7e93fcf5506b9d68f303b1fad48035daa18d
Jefferson Donizete do Nascimento <jdn1937@hotmail.com>
2022-11-03 10:57:03 -0300

    Requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,33 @@
-const data = require('../data/zoo_data');
+const { employees, species } = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const funcionarios = () => employees.map((employee) => {
+  const animais = species.filter((animal) => {
+    if (employee.responsibleFor.includes(animal.id)) {
+      return true;
+    }
+    return false;
+  });
+  return {
+    id: employee.id,
+    fullName: `${employee.firstName} ${employee.lastName}`,
+    species: animais.map((specie) => specie.name),
+    locations: animais.map((specie) => specie.location),
+  };
+});
+console.log(funcionarios());
+function getEmployeesCoverage(dado) {
+  if (dado === undefined) {
+    return funcionarios();
+  }
+  const { name, id } = dado;
+  if (name !== undefined) {
+    return funcionarios().find((funcionario) => funcionario.fullName.includes(name));
+  }
+  const confirmaId = funcionarios().find((funcionario) => funcionario.id === id);
+  if (confirmaId !== undefined) {
+    return confirmaId;
+  }
+  throw new Error('Informações inválidas');
 }
 
 module.exports = getEmployeesCoverage;
